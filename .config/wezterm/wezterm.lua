6c192634dc34778e74bf87245c18c63fd0ef6d49
Vivek <thistovivek@gmail.com>
2025-09-29 08:49:10 +0530

    Added wezterm configs

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,47 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+return {
+	color_scheme = "One Dark (Gogh)",
+
+	-- General appearance
+	font = wezterm.font_with_fallback({
+		"JetBrains Mono",
+		"Fira Code",
+	}),
+	font_size = 14.0,
+	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
+	window_background_opacity = 0.99,
+	enable_tab_bar = false, -- hides the tab bar
+
+	-- Keybindings
+	keys = {
+		-- New tab (CTRL+i)
+		{ key = "w", mods = "CTRL", action = act.SpawnTab("DefaultDomain") },
+
+		-- Splits
+		{ key = "v", mods = "CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+		-- Pane navigation (Alt + Vim keys)
+		{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+
+		-- Close current pane
+		{ key = "q", mods = "CTRL", action = act.CloseCurrentPane({ confirm = true }) },
+
+		-- Cycle tabs (even without tab bar visible!)
+		{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
+		{ key = "Tab", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(-1) },
+	},
+}
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # dotfiles
 
 My dotfiles managed using GNU stow.
+
+--- 
+
+A ignore file can be made using the .stow-local-ignore file and naming the files/folders to be ignored.
+
+--- 
+
+# Installation 
+
+Installation can be done by cloning the repo and following the below steps 
+
+```bash
+git clone https://github.com/DynamiteMonki/dotfiles ~/dotfiles 
+cd dotfiles
+stow .
+```
+
+and important cmd,
+
+```bash
+stow --adopt .
+```
