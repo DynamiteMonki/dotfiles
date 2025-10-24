33745c9b020e4984fc1fc76a9f475771bb0126c9
DynamiteMonki <thistovivek@gmail.com>
2025-10-24 19:49:55 +0530

    Friday 24 October 2025 07:49:55 PM IST

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -1,13 +1,4 @@
 return {
 	"folke/which-key.nvim",
-	tag = "v2.1.0", -- Pin to v2
 	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	config = function()
-		local wk = require("which-key")
-		wk.setup({})
-	end,
 }
