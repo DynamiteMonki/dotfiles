77206170d1b1b31bf9cbacb93d508e25cd7c0db3
DynamiteMonki <thistovivek@gmail.com>
2025-10-24 10:18:52 +0530

    Friday 24 October 2025 10:18:52 AM IST

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+  'stevearc/conform.nvim',
+  config = function ()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = {"stylua"},
+      },
+    })
+    -- autocmd
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function (args)
+        require("conform").format({bufnr = args.buf})
+      end
+    })
+
+    vim.keymap.set("n", "<leader>f", function (args)
+      require("conform").format({bufnr = args.buf})
+    end,
+    {desc = "Format a file"}
+  )
+  end
+}
