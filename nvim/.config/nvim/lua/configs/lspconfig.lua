fa49a735718400977efa9f05f0d6eab658bc7816
DynamiteMonki <thistovivek@gmail.com>
2025-10-25 15:45:36 +0530

    Saturday 25 October 2025 03:45:36 PM IST

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua_ls", "basedpyright", "clangd" }
+local servers = { "html", "cssls", "lua_ls", "basedpyright", "clangd", "deno" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
