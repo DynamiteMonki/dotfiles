aed2d8b070fab202d2618d9fbeea5649130678ad
DynamiteMonki <thistovivek@gmail.com>
2025-10-25 15:18:11 +0530

    Finally a proper configureable config that's it

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').setup({
+      pickers = {
+        buffers = {
+          show_all_buffers = true,
+          sort_lastused = true,
+          theme = "dropdown",
+          previewer = false,
+          mappings = {
+            i = {
+              ["<c-d>"] = "delete_buffer",
+            }
+          }
+        }
+      }
+    })
+  end
+}
