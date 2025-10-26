5cf01a8dc9656cb74c1c72dbe170786787662ced
DynamiteMonki <thistovivek@gmail.com>
2025-10-26 09:24:44 +0530

    Sunday 26 October 2025 09:24:44 AM IST

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,17 +1,26 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font({
+	family = "JetBrains Mono",
+	harfbuzz_features = {
+		"calt=1", -- Contextual alternatives
+		"clig=1", -- Contextual ligatures
+		"liga=1", -- Standard ligatures
+		"zero", -- Slashed zero
+		"ss01", -- Stylistic set (varies by font)
+	},
+})
+
 config.font_size = 14.0
-config.color_scheme = 'One Dark (Gogh)'
+config.color_scheme = "One Dark (Gogh)"
 config.window_background_opacity = 0.97
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 config.enable_tab_bar = false
-
 
 return config
