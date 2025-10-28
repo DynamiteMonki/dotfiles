42e7bfce05db16067fcd9843e73495f1b7fb865d
DynamiteMonki <thistovivek@gmail.com>
2025-10-28 17:50:19 +0530

    Changed some options realted to ligatures in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,15 +4,17 @@ local config = {}
 config.font = wezterm.font({
 	family = "JetBrains Mono",
 	harfbuzz_features = {
-		"calt=0", -- Contextual alternatives
+		"calt=1", -- Contextual alternatives
 		"clig=1", -- Contextual ligatures
 		"liga=1", -- Standard ligatures
 		"zero", -- Slashed zero
 		"ss01", -- Stylistic set (varies by font)
 	},
+	weight = "Regular",
+	-- italic = true,
 })
 
-config.font_size = 14.0
+config.font_size = 14.5
 config.color_scheme = "One Dark (Gogh)"
 config.window_background_opacity = 0.97
 config.window_padding = {
