55c1a9d8cc50336939214e26ddb8cff114ca6e98
DynamiteMonki <thistovivek@gmail.com>
2025-11-18 17:10:54 +0530

    Planing to get neovim config for nvchad back

--- nvim/.config/nvim/lua/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-mini/mini.nvim",
-	config = function()
-		require("mini.pairs").setup()
-		require("mini.pick").setup()
-		require("mini.icons").setup()
-		require("mini.statusline").setup()
-	end,
-}
