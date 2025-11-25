46310ee35debc366180cfe1fe9b287f9df5b211a
DynamiteMonki <thistovivek@gmail.com>
2025-11-25 18:27:00 +0530

    Added some functionality to neovim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,6 +8,9 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.winborder = "rounded"
 opt.signcolumn = "yes"
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.undodir = vim.fn.stdpath("state") .. "/undo"
 
 -- mapping options
 vim.g.mapleader = " "
