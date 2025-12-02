82358bea3161b9898447cb6f1e1df785ca92c727
DynamiteMonki <thistovivek@gmail.com>
2025-12-02 18:09:06 +0530

    Added some changes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
+	"Shatur/neovim-ayu",
 	config = function ()
-		vim.cmd("colorscheme catppuccin")
+		local color = require("ayu.colors")
+		color.generate(true)
+		vim.cmd("colorscheme ayu")
 	end
 }
